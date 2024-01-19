fd83a9f3d34005da1dccde90293a23f6343d9660
AnaFilipaNogueira <65410301+AnaFilipaNogueira@users.noreply.github.com>
2024-01-19 20:02:19 +0000

    Update construct_json.py

--- fixmypose/construct_json.py
+++ fixmypose/construct_json.py
@@ -19,3 +19,15 @@ f_data.close()
 json_object = json.dumps(list_of_items, indent=4)
 with open("/nas-ctm01/datasets/public/posefix_dataset/" + split + ".json", "w") as outfile:
     outfile.write(json_object)
+
+with open("/nas-ctm01/datasets/public/posefix_dataset/" + split + ".json", "w") as f1:
+    data_saved = json.load(f1)
+
+for i in data_saved:
+    i['sents_0'] = str(i['uid'])
+    
+with open("/nas-ctm01/datasets/public/posefix_dataset/" + split + '.json') as f2:
+    json.dump(data, f2, indent = 4)
+
+f1.close()
+f2.close()
